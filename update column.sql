7b83f7a50547705730d582f24b7e5f6ad18087a8
nupoor <nupoorktech@gmail.com>
2026-01-07 14:00:06 +0530

    Delete update column.sql

--- update column.sql
+++
@@ -1,9 +0,0 @@
--- UPDATING THE COLUMN
-/*UPDATE Sales
-SET ProfitMargin = (Profit / Sales) * 100
-WHERE Sales > 0 */
-
--- See all columns in your table
-SELECT COLUMN_NAME 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME = 'Sales';
